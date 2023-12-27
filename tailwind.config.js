bdd54b7a0fb1abdda2721851d158331ad875c1d9
vincalaudia <claudiajovinca@gmail.com>
2023-12-27 20:10:02 +0700

    minor update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}", // For pages
+    "./Components/**/*.{js,ts,jsx,tsx}", // For components in 'my-porto/Components'
+    "./styles/**/*.css", // If you use Tailwind in CSS files in 'styles' directory
+    // Add any other directories where you might use Tailwind CSS classes,
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
