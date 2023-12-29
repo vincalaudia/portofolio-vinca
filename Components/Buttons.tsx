8f8fdaca3605c246c9e26a872d2a876ff9cce7a9
vincalaudia <claudiajovinca@gmail.com>
2023-12-30 04:09:32 +0700

    minor edit

--- Components/Buttons.tsx
+++ Components/Buttons.tsx
@@ -11,7 +11,6 @@ const Buttons = ({ buttonLabel, icon, link }: ButtonProps) => {
   return (
     <a
       href={link}
-      target="_blank"
       rel="noopener noreferrer"
       className="px-5 py-3 md:px-8 md:py-4 my-[2px] hover:bg-[#304b54c2] transition-all duration-200 text-[11px] lg:text-[18px] font-semibold text-white bg-[#304b5461] flex items-center space-x-2 border-solid border-2 rounded-[400px] border-[#FFFFFF] z-100"
     >
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -41,9 +41,7 @@ const Hero = () => {
             <Buttons
               icon={true}
               buttonLabel={"Check Out My Projects"}
-              link={
-                "https://figma.com/proto/WBYMbm0NI9ShUNojqZhKJH/Untitled?page-id=1%3A4&type=design&node-id=49-2504&viewport=1949%2C1406%2C0.28&scaling=min-zoom&starting-point-node-id=49%3A2504&show-proto-sidebar=1"
-              }
+              link={"/MyProjects"}
             />
           </div>
         </div>
