3e4a523e5ab61b37d2f1d0a31296b3279d7fd460
vincalaudia <claudiajovinca@gmail.com>
2024-05-17 02:21:36 +0700

    Project Page Updated

--- Components/CarouselImage.tsx
+++ Components/CarouselImage.tsx
@@ -1,29 +1,59 @@
-import React, { Component } from "react";
-import Image from "next/image";
+import React from "react";
 import Slider from "react-slick";
-import Card from "./Card";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
-export default class Carousel extends Component {
-  render() {
-    const settings = {
-      className: "center",
-      centerMode: true,
-      infinite: true,
-      centerPadding: "20px",
-      slidesToShow: 1,
-      speed: 500,
-      dots: true,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1700,
-      cssEase: "linear",
-    };
-    return (
-      <div className="bg-[#374a6035] w-full mx-auto overflow-wrap">
-        <Slider {...settings}></Slider>
-      </div>
-    );
-  }
-}
+export type ImagesProps = {
+  images: string[];
+};
+
+const CarouselImage = ({ images }: ImagesProps) => {
+  const settings = {
+    customPaging: function (i: number) {
+      return (
+        <div className="relative w-[12vw] h-auto lg:w-[8vw]">
+          <Image
+            src={images[i]}
+            alt="user"
+            layout="responsive"
+            width={160} // adjust as needed
+            height={90} // adjust as needed
+            className="w-full object-contain rounded-sm md:rounded-md"
+          />
+        </div>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {images.map((url, index) => (
+          <div key={index}>
+            <div className="relative mx-auto mt-[13vh] w-[97vw] h-auto md:mt-[6vh] md:w-[90vw] lg:w-[65vw]">
+              <Image
+                src={url}
+                alt="user"
+                layout="responsive"
+                width={160} // adjust as needed
+                height={90} // adjust as needed
+                className="relative w-fit rounded-xl radius-2xl"
+              />
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default CarouselImage;
